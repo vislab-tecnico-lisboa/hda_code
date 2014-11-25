e79e2c254df38ce6156dd1ff5fc4d1e96481f316
Dario Figueira <wurmdario@gmail.com>
2014-11-25 11:42:10 +0000

    removed resizing masks to orginal size, removed the limiting access to waitbar, added draft of where to put algorithms that need all the test data at once

--- detection_reid_pipeline/reIdentificationWrapper.m
+++ detection_reid_pipeline/reIdentificationWrapper.m
@@ -1,6 +1,6 @@
 function reIdentificationWrapper()
 %Reads filtered detections and generated ReId output 
-% MORE COMMENTS
+% TODO: MORE COMMENTS
     
 	declareGlobalVariables,
 
@@ -19,10 +19,14 @@ function reIdentificationWrapper()
         % trainDataStructNoTestCamera = trainingDataStructure([trainingDataStructure.camera] ~= testCamera);
         % numberTrainingPedestrians = length(unique([trainDataStructNoTestCamera.personId]));
         
-        %Work on one file at a time
         filteredCropsDirectory = [experimentDataDirectory sprintf('/camera%02d',testCamera) '/FilteredCrops'];
+
+        % Loading allF here (before the if exist test) just to make it easy
+        % to re-run MultiView
+        filteredCropsMat = dlmread([filteredCropsDirectory '/allF.txt']);
+        nFiles=size(filteredCropsMat,1);
+        
         reIdsDirectory         = [experimentDataDirectory sprintf('/camera%02d',testCamera) '/ReIds_' reIdentifierName];
-        featuresDirectory      = [thisDetectorDetectionsDirectory sprintf('/camera%02d',testCamera) '/Detections'];
         if(~exist(reIdsDirectory,'dir')), mkdir(reIdsDirectory); end;
         if(recomputeAllCachedInformation)
             warning('off','MATLAB:DELETE:FileNotFound')
@@ -40,92 +44,95 @@ function reIdentificationWrapper()
         fprintf(fid,'Format: \n');
         fprintf(fid,'camera#, frame#, x0, y0, width, height, estimatedId1, estimatedId2, ...\n');
         fclose(fid);
-
-        filteredCropsMat = dlmread([filteredCropsDirectory '/allF.txt']);
-        % testFeatures = load([featuresDirectory '/featHSV(4 Parts)[10,10,10]eq.mat']);
-        nFiles=size(filteredCropsMat,1);
         
         % Loading pre-computed body-part masks
         bodypartmaksksDirectory      = [thisDetectorDetectionsDirectory sprintf('/camera%02d',testCamera) '/Detections'];
-        % No longer care about originalSize
-%         if exist([bodypartmaksksDirectory '/pmskset4_originalSize.mat'],'file')
-%             masks = load([bodypartmaksksDirectory '/pmskset4_originalSize.mat']);
-%         else
-%             warning(['Body-part masks of original size do not exist, probably because they would be too large (several GB).' ...
-%                 ' Using sub-sampled masks (128x64) instead.'])
-            masks = load([bodypartmaksksDirectory '/pmskset4_128x64.mat']);
-%         end
+        masks = load([bodypartmaksksDirectory '/pmskset4_128x64.mat']);
         masks = masks.pmskset;
         if size(masks,1) ~= nFiles
             error(['Number of masks (' int2str(size(masks,1)) ') not equal to number of filtered crops (' int2str(nDetections) '). Maybe they were computed with crowd detections?'])
         end
         
-        wbr = waitbar(0, ['ReIdentifying on camera ' int2str(testCamera) ', image 0/' int2str(nFiles)]);
-        dividerWaitbar=10^(floor(log10(nFiles))-1); % Limiting the access to waitbar
-        allRMatToSave = zeros(nFiles,6+nPed);
-        for count=1:nFiles
-            % if (round(count/dividerWaitbar)==count/dividerWaitbar) % Limiting the access to waitbar
-            % but the re-id classifiers so far have been slow enough that
-            % we don't need to limit the access to waitbar
-                waitbar(count/nFiles, wbr, ['ReIdentifying on camera ' int2str(testCamera) ', image ' int2str(count) '/' int2str(nFiles)]);
-            % end
-            dataLine = filteredCropsMat(count,:);
-            if dataLine(7) == 0    % if "active" bit turned off
-                continue,          % leave line in matrix empty
-            end
-            frame = dataLine(2);
-            bb = dataLine(3:6);
+        if strncmp(reIdentifierName,'MultiView',9)
+            % Special case: algorithms that take as input the whole test set
+            % before outputting the re-identification classifications
             
-            if ~offlineCrop_and_not_OnTheFlyFeatureExtraction
-                % ON-THE-FLY IMAGE CROPPING
-                subImage = getFrameAndCrop(testCamera, frame, bb);
-            else % if cropped offline
-                subImage = imread([filteredCropsDirectory sprintf('/F%06d.png',count)]);
-            end
-            % Padding image to 2 per 1 ratio, to match the size of
-            % body-part masks
-            paddedImage = smartPadImageToBodyPartMaskSize(subImage);            
-                        
-            % Extracting HSV feature vector (10 bin per channel of each of
-            % the 4 parts, totaling 120x1 vector)
-            feature = featureExtractionHandle(paddedImage,masks(count,:));
-            
-            % RE-ID classification, receives feature vector for test
-            % sample, and trainingDataStructure that contains all
-            % pertaining to the training samples (more info on createTrainStructure.m).
-            dist1toPedsPIDsI = reIdentifierHandle(feature,trainingDataStructure);
-                      
-            allRMatToSave(count,:) = [dataLine(1:6) dist1toPedsPIDsI];
-            
-            % DEBUG VISUALIZATION
-            % VISUALIZE OFFLINE CROPPED IMAGE, ONLINE CROPPED IMAGE, PADDED
-            % IMAGE, AND BODY-PART MASKS OVERLAYED ON THE PADDED IMAGE
-            %figure(234),
-            %subplot(1,5,1)
-            %imshow(subImage),            title('Online Crop'),
-            %
-            %FXXXpng = [filteredCropsDirectory '/' sprintf('F%06d.png',count)];
-            %if exist(FXXXpng,'file')
-            %    subplot(1,4,2)
-            %    imshow(imread(FXXXpng))
-            %    title('Cropped offline')
-            %end
-            %
-            %subplot(1,5,3)
-            %imshow(paddedImage), title('Padded'),
-            %
-            %subplot(1,5,4)
-            %imshow(paddedImage), title('Masked')
-            %hold on
-            %plotBodyPartMasks(paddedImage,masks(count,:));
-            %hold off,
-            %
-            %subplot(1,5,5)
-            %bar(HSV),
-            % END DEBUG VISUALIZATION
+            testDataStructure = createTestStructure(testCamera);
+            filteredTestStruct = testDataStructure(logical(filteredCropsMat(:,7)));
 
+            % if we haved a function
+            rankedList = reIdentifierHandle(trainingDataStructure,filteredTestStruct,filteredCropsMat); % OUTPUT: rankedList
+            % if we haved a script
+            %eval(reIdentifierName)
+            
+            allRMatToSave = zeros(nFiles,6+nPed);
+            allRMatToSave(:,1:6) = filteredCropsMat(:,1:6);
+            allRMatToSave(:,7:end) = rankedList'; 
+        else
+            % Regular case, re-identifier takes one test sample and outputs one
+            % classification vector
+            wbr = waitbar(0, ['ReIdentifying on camera ' int2str(testCamera) ', image 0/' int2str(nFiles)]);
+            allRMatToSave = zeros(nFiles,6+nPed);
+            for count=1:nFiles
+                waitbar(count/nFiles, wbr, ['ReIdentifying on camera ' int2str(testCamera) ', image ' int2str(count) '/' int2str(nFiles)]);
+                dataLine = filteredCropsMat(count,:);
+                if dataLine(7) == 0    % if "active" bit turned off
+                    continue,          % leave line in matrix empty
+                end
+                frame = dataLine(2);
+                bb = dataLine(3:6);
+
+                if ~offlineCrop_and_not_OnTheFlyFeatureExtraction
+                    % ON-THE-FLY IMAGE CROPPING
+                    subImage = getFrameAndCrop(testCamera, frame, bb);
+                else % if cropped offline
+                    subImage = imread([filteredCropsDirectory sprintf('/F%06d.png',count)]);
+                end
+                % Padding image to 2 per 1 ratio, to match the size of
+                % body-part masks
+                paddedImage = smartPadImageToBodyPartMaskSize(subImage);            
+
+                % Extracting HSV feature vector (10 bin per channel of each of
+                % the 4 parts, totaling 120x1 vector)
+                feature = featureExtractionHandle(paddedImage,masks(count,:));
+
+                % RE-ID classification, receives feature vector for test
+                % sample, and trainingDataStructure that contains all
+                % pertaining to the training samples (more info on createTrainStructure.m).
+                dist1toPedsPIDsI = reIdentifierHandle(feature,trainingDataStructure);
+
+                allRMatToSave(count,:) = [dataLine(1:6) dist1toPedsPIDsI];
+
+                % DEBUG VISUALIZATION
+                % VISUALIZE OFFLINE CROPPED IMAGE, ONLINE CROPPED IMAGE, PADDED
+                % IMAGE, AND BODY-PART MASKS OVERLAYED ON THE PADDED IMAGE
+                %figure(234),
+                %subplot(1,5,1)
+                %imshow(subImage),            title('Online Crop'),
+                %
+                %FXXXpng = [filteredCropsDirectory '/' sprintf('F%06d.png',count)];
+                %if exist(FXXXpng,'file')
+                %    subplot(1,4,2)
+                %    imshow(imread(FXXXpng))
+                %    title('Cropped offline')
+                %end
+                %
+                %subplot(1,5,3)
+                %imshow(paddedImage), title('Padded'),
+                %
+                %subplot(1,5,4)
+                %imshow(paddedImage), title('Masked')
+                %hold on
+                %plotBodyPartMasks(paddedImage,masks(count,:));
+                %hold off,
+                %
+                %subplot(1,5,5)
+                %bar(HSV),
+                % END DEBUG VISUALIZATION
+
+            end
+            close(wbr);
         end
-        close(wbr);
         
         dlmwrite([reIdsDirectory '/allR.txt'],allRMatToSave);
         cprintf('*[1,0,1]',['Saved allR.txt to ' reIdsDirectory '\n'])        
