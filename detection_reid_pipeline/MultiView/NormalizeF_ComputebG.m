fa3fdc06a0e9ba346ae963df7b809299e41f240e
Dario Figueira <wurmdario@gmail.com>
2015-01-28 16:58:51 +0000

    No longer trying to use sparse when not enough memory

--- detection_reid_pipeline/MultiView/NormalizeF_ComputebG.m
+++ detection_reid_pipeline/MultiView/NormalizeF_ComputebG.m
@@ -33,12 +33,21 @@ else
     display(['running non-vectorized kernel code, kernelType ' kernelType])
     try % to allocate bG, if too large for memory, use sparse()
         bG = zeros(m*(u+l));
+%         bG2 = zeros(m*(u+l));
     catch me
-        % TODO: COMPUTE EXPECTED MEMORY USAGE AND DISPLAY (bG only has half
-        % its elements filled by zeros)
-        cprintf('error', [me.message ' allocating bGs.'])
-        cprintf(' Using sparse\n')
-        bG = sparse(m*(u+l),m*(u+l));
+        try
+            cprintf('error', 'Error: ');
+            warning([me.message ' allocating bGs. Needed ' num2str(m*(u+l)*m*(u+l)/1024/1024/1024) 'GB..  Trying Single type' ])
+            bG = zeros(m*(u+l),'single');            
+        catch me
+            % TODO: COMPUTE EXPECTED MEMORY USAGE AND DISPLAY (bG only has half
+            % its elements filled by zeros)
+            cprintf('error', 'Error: ');
+            error([me.message ' ... Don''t even bother using sparse, you have a kron later on in the optimization that needs even more memory...'  ])
+            warning([me.message ' allocating bGs. Needed ' num2str(m*(u+l)*m*(u+l)/1024/1024/1024/2) 'GB..  Using sparse' ])
+            % bG = sparse(m*(u+l),m*(u+l));
+            bG = spalloc(m*(u+l),m*(u+l), m*(u+l)*m*(u+l)/m);
+        end
     end
     
     % TODO: bG is symmetric, take advantage of it below
@@ -57,8 +66,26 @@ else
             
             bG(1+(i-1)*m : i*m , 1+(j-1)*m : j*m) = G;
         end
-        waitbar(i/(u+l),bGh);
+        %for j=1:(u+l) % For all samples
+% TRIED TO SAVE TIME, ASSUMING bG IS SYMMETRIC (NOT SURE), BUT NOT WORKING        
+%         for j=i:(u+l) % For all samples
+%             
+%             G = zeros(m,m);
+%             for mIt=1:m % For all views
+%                 K = Kernel(Fmatrix{mIt,1}(i,:), Fmatrix{mIt,1}(j,:), 1, Fmatrix{mIt,2}, i, j); % computing bG with sigmas == 1
+%                 % DEBUG computing bG with sigma == s
+%                 % K = Kernel(Fmatrix{mIt,1}(i,:), Fmatrix{mIt,1}(j,:), s(mIt), Fmatrix{mIt,2}, i, j); 
+%                 G(mIt,mIt) = K;
+%             end
+%             
+%             %bG(1+(i-1)*m : i*m , 1+(j-1)*m : j*m) = G;
+%             bG2(1+(j-1)*m : j*m , 1+(i-1)*m : i*m) = G;
+%         end
+        waitbar(i/(u+l),bGh,['computing bG ' int2str(i) ' of ' int2str(u+l) ]);
     end
     close(bGh),
+    inSparcity(bG)
+%     inSparcity(bG2)
+%     display(['bG-bG2: ' inSparcity(bG-bG2)])
 end
 toc(bGtstart),
