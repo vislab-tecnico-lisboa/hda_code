47055e68f12ee7744df99f69109cb94810fb3bf0
Dario Figueira <wurmdario@gmail.com>
2015-05-27 14:52:39 +0100

    corrected bug, test image should be in the beggining of the distance matrix

--- detection_reid_pipeline/BhattDist1toAll.m
+++ detection_reid_pipeline/BhattDist1toAll.m
@@ -5,7 +5,7 @@ function  distVT1 =  BhattDist1toAll(F97testIm, F96)
 avoid0 = realmin;
 
 % VTd = double([F96; F97(testIm,:)]);
-VTd = double([F96; F97testIm]);
+VTd = double([F97testIm; F96]);
 [nImages,histLen] = size(VTd); % VT should be nImages x histLen
 normVec = avoid0 + sum(VTd,2); % vector with sum of each ped hist
 hists_i = sqrt(VTd ./ normVec(:,ones(1,histLen))); % each hist normalized to 1, sqrt'ed
