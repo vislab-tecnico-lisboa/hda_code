b2f1e9dc8287ffd89a7fa4668a1dc4c044dc347a
Dario Figueira <wurmdario@gmail.com>
2015-05-27 16:36:34 +0100

    outsourced the code

--- detection_reid_pipeline/fullPipeLineScript.m
+++ detection_reid_pipeline/fullPipeLineScript.m
@@ -54,38 +54,4 @@ Verify_existance_of_detections_and_create_experiment_folders,
 
 %% Pipe-line computation proper
 
-% Average all test camera samples case
-if isempty(trainCameras) && isempty(testCameras)
-    % Run one experiment per test camera with all the
-    % other cameras as train, for all test cameras. Then, concatenate all
-    % the sample results and compute a global CMC
-    [trainingDataStructure, allTrainingDataStructure] = createTrainStructure(0);
-    pedNum = length(unique([allTrainingDataStructure.personId]));
-
-    cameras = [17 18 19 40 50 53 54 56 57 58 59 60];
-    allGs = [];
-    for irun = 1:length(cameras)
-        testCameras = cameras(irun);
-        trainCameras = cameras(cameras ~= testCameras);
-        
-        pipelineComputationProper,
-        if size(allG,2) < pedNum+3
-            % making the allG matrixes the same size for concatenation
-            allG(1,pedNum+3) = 0;
-        end
-        allGs = [allGs; allG];
-        
-        %evaluatorCMC(),
-    end        
-    evaluatorCMC('all',allGs)
-
-    
-% Only one test camera case    
-else
-    % Plot one CMC per test camera specified, assume training cameras don't
-    % overlap with test cameras specified.
-    pipelineComputationProper,
-
-    evaluationProper,
-    
-end
+pipeLineComputationProperWithALL,
