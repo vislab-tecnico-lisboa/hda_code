4bd4e39cd2ca86ddef33da12111eb6241cbb76e4
Dario Figueira <wurmdario@gmail.com>
2015-05-30 13:21:17 +0100

    added comments, featureExtractionMethod, example of running all test cameras and averaging resutls, and example of using other featuresextractionmethods

--- detection_reid_pipeline/setUserDefinedExperimentParameters.m
+++ detection_reid_pipeline/setUserDefinedExperimentParameters.m
@@ -30,17 +30,41 @@ detectorName = 'AcfInria';% Possible options
                           % 'GtAnnotationsClean': use manual annotations of only fully visible pedestrians as bounding box inputs   
                           % 'GtAnnotationsAll'  : use manual annotations of all pedestrians as bounding box inputs   
                           % YourOwnDetector     : Put your detections in [hdaRootDirectory 'hda_detections/' detectorName '/camera##/Detections/'] 
+                          
 useMutualOverlapFilter  = 1;
 maximumOcclusionRate    = 0.3; % Set it to greater than 1 for disabling the Occlusion filter
 useFalsePositiveClass   = 1;
 trainingSetPath         = [hdaRootDirectory '/hda_sample_train_data'];
+
 reIdentifierHandle      = @BhattacharryaNNReId; % Implemented example of NN classifier with Bhattacharrya distance.
 % Other options:
 %    @randomReId : determines the re-identification classifications randomly from the training class list
 %    @MSCR_NN_ReId : MSCR feature nearest neighbor computation
+%    @SDALF_ReId : SDALF's nearest neighbor combination of MSCR, EpiTexture and weighted HSV histogram 
+%    @MultiViewold_BVT_HSV_Lab_MR8_LBP : MutliView classifier that combines those 5 features 
+
 featureExtractionHandle =  @extractHSVfromBodyParts; % Implemented example of HSV histogram extraction
 % Other options:
 %    @extractMSCR 
+%    @extractBVT - a variant of HSV (2D H and S histogram, 1D V histogram, and 1 bin for the near-black pixels) 
+%    @extractLab
+%    @extractLBP
+%    @extractMR8
+%    @extractSDALF
+%    @extractBVT_HSV_Lab_MR8_LBP - to be used with MultiView classifier (extracts the 5 features and puts them in a structure) 
+
+% Feature extraction method, default '4parts', that uses Pictorial
+% Structures to detect 6 body parts [head | torso | thigh | shin | shin | foreleg]
+% and joints together the shins in one area, and the thighs in one area:
+% [head | torso | thighs | shins]
+featureExtractionMethod = '4parts';
+% Other options:
+%    'fullbody' - joining into one masks all the body-part masks
+%    '2parts'   - does a search in the fullmask for the point that
+%               maximizes the bhattacharyya distance between the upper and lower color
+%               histogram (BVT feature), and divide the fullbody mask in two at that point.   
+%    '6parts'
+
 
 if strcmp(experimentVersion,'001')
     %use all default parameters
@@ -93,7 +117,22 @@ elseif strcmp(experimentVersion,'Toy8_BVT')
     reIdentifierHandle = @BhattacharryaNNReId;
     featureExtractionHandle =  @extractBVT; 
     trainingSetPath         = [hdaRootDirectory '/hda_sample_train_data_toy8'];    
-        
+
+% Set trainCameras and testCameras to [] to run 12 experiments and
+% display an average CMC for all samples in all experiments. Each
+% experiment with one camera as test and all the others as train. 
+% (Note that what is displayed is not an CMC that is the average of 12
+% CMCs, but an CMC computed from the sum of all the re-identifications for
+% all samples in all 12 experiments)
+elseif strcmp(experimentVersion,'MANUALclean_allcam_HSV_2parts')
+    trainCameras  = [];
+    testCameras = []; 
+    detectorName = 'GtAnnotationsClean';
+    useFalsePositiveClass = 0;
+    useMutualOverlapFilter = 0;
+    featureExtractionMethod = '2parts';
+    
+    
 else
     error(['Unrecognized experimentVersion: ' experimentVersion])
 end
